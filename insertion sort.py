558cdea2d0407ac65f74b32bce38eebf08f443a3
tusharta07 <54472089+tusharta07@users.noreply.github.com>
2019-10-28 17:31:24 +0530

    Add files via upload

---
+++ insertion sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n=len(arr)
+    for i in range(1, n):
+        for j in range(i-1,-1,-1):
+            if arr[j]>arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            else:
+                break
+
+arr= [int(k) for k in input().split()]
+bubble_sort(arr)
+print(arr)
